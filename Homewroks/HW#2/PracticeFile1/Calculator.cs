7a868e88e66e0f4e8e893fb499cea067a1e81480
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-03-08 02:24:18 +0500

    class library named calculator

---
+++ Homewroks/HW#2/PracticeFile1/Calculator.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculator
+{
+    public class myCalculator
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        public int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+        public int Division(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
