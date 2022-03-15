30ded7791110835d86d6ba46e4fa492cb7c44e42
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-03-15 22:52:17 +0500

    Create loginBO.cs

---
+++ Assignments/ATM_SoftwareConsoleBased_EAD_A1/loginBO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login_BO
+{
+    public class loginBO
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
