6372ac0dbb9d4be3673b41c3db07385ae02c4fed
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-04-11 19:19:26 +0500

    Update LINQ-methodSyntax.cs

--- Homewroks/HW#7/LINQ-methodSyntax.cs
+++ Homewroks/HW#7/LINQ-methodSyntax.cs
@@ -1,4 +1,70 @@
 /*
+SQL queries are used against those data objects that implements the IEnumerable interfaces.
+
+----------WHAT IS IENUMERABLE INTERFACE??
+IEnumerable interface is used when we want to iterate among our classes using a foreach loop.
+The IEnumerable interface has one method, GetEnumerator,
+that returns an IEnumerator interface that helps us to iterate among the class using the foreach loop.
+
+
+This is Query syntax in LINQ.
+
+var query = from data in dataset
+            where (filter condtion)
+            orderby (sort)
+            select things
+
+foreach (var value in query)
+{
+    //process
+}
+  
+ 
+ */
+
+
+using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //1 - define data source
+            string[] cities = new string[] { "Lhr", "Okaraaa" };
+
+            //2 - define query1
+            var query1 = from c in cities
+                        select c;
+
+            //2 - define query
+            var query2 = from c in cities
+                         where c.Length>5
+                         select c;
+
+
+            //3 - execution of query
+            foreach (var city in query1)
+            {
+                Console.WriteLine(city);
+            }
+
+            //3 - execution of query2
+            foreach (var city in query2)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
+
+
+
+/*
  * 
  * -------------This is method syntax to use LINQ.
  * -------------Next we will see query syntax to use LINQ.
@@ -36,6 +102,12 @@ namespace LINQ
             //2 - query using lamda expression
             //lamda expression do not use return keyword and we do not need to specify the parameter data type as well.
             var query = cities.Where(cityName => cityName.Length > 5);     //extension method - Where is not any method defined in string array but in system.linq space.
+
+
+            //Here query has its data type IEnumerator because it is not executing,just defined. Query executes when you explicitly
+            //define data collection. i.e. toList. This method defines and executes query as well.
+            //var query = cities.Where(cityName => cityName.Length > 5).ToList();
+
 
             //where clause se jo result filter ho k aya us ko kis tarha ap print krwana chah ry? (yahan wo pehle 3 characters hen)
             var query2 = cities
