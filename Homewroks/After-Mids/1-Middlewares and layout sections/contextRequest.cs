e1a7a42aca174da46bfd250da80d2b8ec5613034
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-05-21 12:49:24 +0500

    Create contextRequest.cs

---
+++ Homewroks/After-Mids/1-Middlewares and layout sections/contextRequest.cs
@@ -0,0 +1,60 @@
+//middleware basics
+/*Difference b/w syncronous(blocking/non-parallel working) and asynchronous(no blocking/parallel working)*/
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddleWares
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        //this method is used to create middleware.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //after deleting bultin code, creating custom middleware
+            //use method is used to create middleware.
+            app.Use(
+                //will learn for async programming.
+                async (context, next) =>              //context parameter is used for response or request.
+                {
+                    context.Response.ContentType = "text/plain";          //mentioning response type
+                    await context.Response.WriteAsync("Aoa! Pakistan");   //modifying response
+                    await next();                                         //to go for next middleware.
+                    await context.Response.WriteAsync("\nI am first middleware.");
+
+                }
+                );
+
+            //creating another middleware
+            app.Use(
+                async (context, next) =>              
+                {      
+                    await context.Response.WriteAsync("\nAoa! Pakistan Again");
+                    await next();
+                    //in browser=>url?someData=abc kr k condition ki verfication kr k dekh len.
+                    if(context.Request.Query["someData"].Equals("abc")){
+                           await context.Response.WriteAsync("\nI am second last middleware.");
+                    }
+                    
+                }
+                );
+            //if you want to terminate middlewre, donot add "await next()" line in middleware.
+            //or
+            app.Run(async (context) =>       //no next parameter because it is last middleware.
+            {
+                await context.Response.WriteAsync("\nI am last middleware.");
+            });
+        }
+    }
+}
