bd8a65032c1a3bfe51801368878e562170a50647
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-08-26 15:20:00 +0500

    Update 8 - Program.cs

--- Homewroks/After-Mids/8 - DependencyInjection/8 - Program.cs
+++ Homewroks/After-Mids/8 - DependencyInjection/8 - Program.cs
@@ -1,9 +1,1 @@
-//open program.cs file and update your ConfigureServices method like this. 
- 
- public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-            
-            //this line tells which interface is implemented by which class.
-            services.AddSingleton<IEmployee, EmployeeRepository>();
-        }
+
