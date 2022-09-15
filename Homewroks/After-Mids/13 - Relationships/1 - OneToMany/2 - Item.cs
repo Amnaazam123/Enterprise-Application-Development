46c9f9d00d6a275148c41e62c4be006a7717cce1
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-09-15 13:18:21 +0500

    Update 2 - Item.cs

--- Homewroks/After-Mids/13 - Relationships/1 - OneToMany/2 - Item.cs
+++ Homewroks/After-Mids/13 - Relationships/1 - OneToMany/2 - Item.cs
@@ -6,6 +6,14 @@ public class Item: FullAuditModel
       public string Description { get; set; }
       public decimal? PurchasePrice { get; set; }
       public int? CategoryId { get; set; } 
-      public virtual Category Category { get; set; }       //
+      public virtual Category Category { get; set; }       //each item belongs to one category.
  
 }
+
+/*
+Now instead of writing inner join queries, you will just write following line of code
+and will get all items with their corresponding categories.
+
+_context.Categories.Items.ToList();
+
+*/
