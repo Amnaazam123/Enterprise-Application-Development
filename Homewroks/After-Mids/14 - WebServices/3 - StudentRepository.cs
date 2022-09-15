cc73ab7920192f1032fb8a40dc797e08968baf97
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-09-15 12:31:46 +0500

    Create 3 - StudentRepository.cs

---
+++ Homewroks/After-Mids/14 - WebServices/3 - StudentRepository.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Services.Models
+{
+    public class StudentRepository
+    {
+        public List<Student> GetAllStudents()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student { ID = 1, Name = "Amna", Age = 20 },
+                new Student { ID = 2, Name = "Iqra", Age = 20 },
+                new Student { ID = 3, Name = "Laiba", Age = 20 },
+                new Student { ID = 4, Name = "Ameena", Age = 20 }
+
+            };
+            return students;
+        }
+    }
+}
