3360b24834a324a2c016d5cc3e1ffc8fc192bf9f
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-09-15 12:31:13 +0500

    Create 2 - Student.cs

---
+++ Homewroks/After-Mids/14 - WebServices/2 - Student.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Services.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
